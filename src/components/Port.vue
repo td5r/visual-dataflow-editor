bb290847ad772e65b6e874010922ad327dab0303
Taleh Didover <dydooov+github@gmail.com>
2022-07-11 18:25:54 +0200

    Add map ports

--- src/components/Port.vue
+++ src/components/Port.vue
@@ -1,17 +1,34 @@
 <script setup>
-defineProps(["def"])
+import { computed, toRefs } from 'vue';
 
-const isMap = computed(() => this.def.type == "map")
-const isStream = computed(() => this.def.type == "stream")
+const props = defineProps(["def"])
+
+const isMap = computed(() => props.def.type == "map")
+const isStream = computed(() => props.def.type == "stream")
 
 </script>
 
 <template>
-<svg:polygon
+<g v-if="isMap"
+class="port--map"
+>
+<Port v-for="(subType, portName, i) in def.map"
+:transform="`translate(${i*(21)} 0)`"
+:def="subType"
+></Port>
+</g>
+
+<g v-else-if="isStream"
+class="port--stream"
+>
+<Port :def="def.stream"
+></Port>
+</g>
+
+<polygon v-else
 class="port"
 :class="def.type"
 points="0,0 20,0 10,18"/>
-
 </template>
 
 <style scoped>
