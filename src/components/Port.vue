74933c7a35420901ab79a834175d653daca03b6d
Taleh Didover <dydooov+github@gmail.com>
2022-07-11 17:48:46 +0200

    Render simple port types

--- src/components/Port.vue
+++ src/components/Port.vue
@@ -1,20 +1,37 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+defineProps(["def"])
+
+const isMap = computed(() => this.def.type == "map")
+const isStream = computed(() => this.def.type == "stream")
+
 </script>
 
 <template>
 <svg:polygon
 class="port"
+:class="def.type"
 points="0,0 20,0 10,18"/>
+
 </template>
 
 <style scoped>
 .port {
+  fill: black;
+}
+.port.string {
+  fill: crimson;
+}
+.port.boolean {
   fill: purple;
 }
+.port.number,.port.primitive {
+  fill: aqua;
+}
+.port.binary {
+  fill: magenta;
+}
+.port.trigger {
+  fill: slategrey;
+}
+
 </style>
